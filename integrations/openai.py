67d6fb8d2e9da83a191b2e871d8fe827e6568222
Anthony-Dev <occamsfreelance@gmail.com>
2023-12-09 16:27:28 -1000

    Comments and Documentation and initial optimization work around stream outputs.

--- integrations/openai.py
+++ integrations/openai.py
@@ -1,25 +1,76 @@
 import queue
 import threading
-from openai import OpenAI
+import logging
+from config import OPENAI_SETTINGS
+from openai import OpenAI, OpenAIError
 
 class OpenAIClient:
+    """
+    A client class for interacting with OpenAI's GPT model.
+
+    This class handles the creation and streaming of responses from the OpenAI API based on recognized text input.
+
+    Attributes:
+        client (OpenAI): The OpenAI client for API interaction.
+        response_queue (queue.Queue): Queue to hold responses from OpenAI.
+        stop_signal (threading.Event): Signal to control the streaming of responses.
+        model (str): The model name for OpenAI API requests.
+    """
     def __init__(self):
-        self.client = OpenAI()
+        """
+        Initializes the OpenAI client with settings from the configuration.
+
+        If an API key is provided in OPENAI_SETTINGS, it uses that key.
+        Otherwise, it defaults to the API key set in the environment variable.
+        """
+        api_key = OPENAI_SETTINGS.get('api_key')
+        if api_key:
+            self.client = OpenAI(api_key=api_key)
+        else:
+            self.client = OpenAI()
         self.response_queue = queue.Queue()
         self.stop_signal = threading.Event()
+        self.model = OPENAI_SETTINGS.get('model', "gpt-3.5-turbo")
 
     def create_completion(self, recognized_text):
-        response = self.client.chat.completions.create(
-            model='gpt-3.5-turbo',
-            messages=[{'role': 'user', 'content': recognized_text}],
-            temperature=0,
-            stream=True
-        )
-        return response
+        """
+        Creates a completion request to the OpenAI API based on recognized text.
+
+        Args:
+            recognized_text (str): The text recognized from the audio input.
+
+        Returns:
+            The response object from the OpenAI API or None if an error occurs.
+        """
+        try:
+            response = self.client.chat.completions.create(
+                model=self.model,
+                messages=[{'role': 'user', 'content': recognized_text}],
+                temperature=0,
+                stream=True
+            )
+            return response
+        except OpenAIError as e:
+            logging.error(f"OpenAI API error: {e}")
+            return None
+        except Exception as e:
+            logging.error(f"Error while creating completion: {e}")
+            return None
 
     def stream_response(self, recognized_text):
+        """
+        Streams the response from the OpenAI API to a queue.
+
+        This method fetches the response for the recognized text and puts each response chunk into a queue.
+
+        Args:
+            recognized_text (str): The text recognized from the audio input.
+        """
         response = self.create_completion(recognized_text)
-        for chunk in response:
-            if self.stop_signal.is_set():
-                break
-            self.response_queue.put(chunk)
+        if response:
+            for chunk in response:
+                if self.stop_signal.is_set():
+                    break
+                self.response_queue.put(chunk)
+        else:
+            logging.info("No response from OpenAI API or an error occurred.")
--- osiris.py
+++ osiris.py
@@ -3,9 +3,15 @@ from vosk import Model
 from audio.audio_processor import AudioProcessor
 import logging
 
+# Configure basic logging for the application
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 def main():
+    """
+    Main function to initiate the audio processing.
+    Retrieves audio settings from the configuration and initializes the AudioProcessor.
+    """
+    # Retrieve audio settings from the configuration file
     sound_device_samplerate = AUDIO_SETTINGS['SOUND_DEVICE_SAMPLERATE'] if AUDIO_SETTINGS['SOUND_DEVICE_SAMPLERATE'] is not None else None
     vosk_model = Model(lang=AUDIO_SETTINGS['VOSK_MODEL'] if AUDIO_SETTINGS['VOSK_MODEL'] is not None else "en-us")
     sound_device_device = AUDIO_SETTINGS['SOUND_DEVICE_DEVICE'] if AUDIO_SETTINGS['SOUND_DEVICE_DEVICE'] is not None else None
@@ -13,10 +19,15 @@ def main():
     audio_in_dump_filename = AUDIO_SETTINGS['AUDIO_IN_DUMP_FILENAME'] if AUDIO_SETTINGS['AUDIO_IN_DUMP_FILENAME'] is not None else None
 
     try: 
+        # Initialize the audio processor with the configuration settings
         audio_processor = AudioProcessor(vosk_model, sound_device_samplerate, sound_device_device, sound_device_blocksize, audio_in_dump_filename)
+        
+        # Start processing the audio stream
         audio_processor.process_stream()
     except KeyboardInterrupt:
+        # Log the termination of the process
         logging.info("\nDone")
 
+# Standard Python idiom for running the main function
 if __name__ == "__main__":
     main()
