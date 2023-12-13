5f9b704f86f0cfb766d60cd3252ff0efb5b19a2a
Anthony-Dev <occamsfreelance@gmail.com>
2023-12-13 11:18:14 -1000

    Ensure quickly enqueued text portions do not result in overlapping filenames. This ensures a consistent sequential stream of audio relative to the provided queue inputs.

--- audio/audio_out.py
+++ audio/audio_out.py
@@ -6,6 +6,7 @@ import queue
 import threading
 import time
 import logging
+import uuid
 
 class AudioOutput:
     def __init__(self):
@@ -39,6 +40,7 @@ class AudioOutput:
         self.play_thread.start()
 
         self.tts_lock = threading.Lock()
+        self.play_lock = threading.Lock()
 
     def text_to_speech(self, text):
         # Synthesize speech from the text
@@ -53,7 +55,8 @@ class AudioOutput:
 
         response = self.client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
 
-        filename = f"temp_audio_{int(time.time())}.wav"
+        filename = f"tmp/audio/temp_audio_{uuid.uuid4()}.wav"
+
         with open(filename, 'wb') as out:
             out.write(response.audio_content)
         return filename
@@ -66,25 +69,20 @@ class AudioOutput:
             text = self.request_queue.get()
             with self.tts_lock:
                 filename = self.text_to_speech(text)
-                if filename:
-                    self.ready_files.put(filename)
+                self.ready_files.put(filename)
                 self.request_queue.task_done()
     
     def play_sequentially(self):
         while True:
             if not self.is_playing() and not self.ready_files.empty():
                 filename = self.ready_files.get()
-
-                # Check if the file exists and retry a few times if it doesn't
-                for _ in range(3):  # retry up to 3 times
-                    if os.path.exists(filename):
-                        self.play_audio_file(filename)
-                        break
-                    time.sleep(0.1)  # wait for 100 milliseconds before retrying
-
-                # Remove the file after playing
-                if os.path.exists(filename):
-                    os.remove(filename)
+                with self.play_lock:
+                    # Check if the file exists and retry a few times if it doesn't
+                    for _ in range(100):  # retry up to 3 times
+                        if os.path.exists(filename):
+                            self.play_audio_file(filename)
+                            break
+                        time.sleep(0.1)  # wait for 100 milliseconds before retrying
 
     def is_playing(self):
         return pygame.mixer.music.get_busy()
@@ -98,8 +96,6 @@ class AudioOutput:
         except pygame.error as e:
             print(f"Error playing audio file: {e}")
 
-        os.remove(filename)
-
     def stop_audio(self):
         # Stops any ongoing audio playback
         pygame.mixer.music.stop()
@@ -108,6 +104,7 @@ class AudioOutput:
         self.stop_audio()
         self.clear_audio_files_queue()
         self.clear_queue()
+        self.clear_orphaned_audio_files()
 
     def clear_audio_files_queue(self):
         while not self.ready_files.empty():
@@ -117,12 +114,26 @@ class AudioOutput:
             except OSError as e:
                 print(f"Error removing file {filename}: {e}")
             self.ready_files.task_done()
+    
+    def clear_orphaned_audio_files(self):
+        # Clear any audio files that may have been left behind
+        for filename in os.listdir('tmp/audio'):
+            try:
+                os.remove("tmp/audio/"+filename)
+            except OSError as e:
+                print(f"Error removing file {filename}: {e}")
 
     def clear_queue(self):
         while not self.request_queue.empty():
             try:
                 self.request_queue.get_nowait()  # Remove all items from the queue
                 self.request_queue.task_done()
+            except queue.Empty:
+                break
+        while not self.ready_files.empty():
+            try:
+                self.ready_files.get_nowait()  # Remove all items from the queue
+                self.ready_files.task_done()
             except queue.Empty:
                 break
 
