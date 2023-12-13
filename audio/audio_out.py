91ee7fdf417f5dd26258070dc628e27ecf70bd61
Anthony L <occamsfreelance@gmail.com>
2023-12-13 11:19:40 -1000

    Voice optimize: Optimizing text queuing in the audio processor to ensure consistent playback. (#5)
    * WIP: Slight optimizations around TTS processing and thread locking to ensure soundbytes are generated in the correct order.
    
    * Ensure quickly enqueued text portions do not result in overlapping filenames. This ensures a consistent sequential stream of audio relative to the provided queue inputs.

--- audio/audio_out.py
+++ audio/audio_out.py
@@ -5,6 +5,8 @@ import pygame
 import queue
 import threading
 import time
+import logging
+import uuid
 
 class AudioOutput:
     def __init__(self):
@@ -28,14 +30,17 @@ class AudioOutput:
         self.audio_encoding = GOOGLE_TTS_CONFIG.get('audio_encoding', texttospeech.AudioEncoding.LINEAR16)
 
         self.request_queue = queue.Queue()
+        self.ready_files = queue.Queue()
         self.audio_thread = threading.Thread(target=self.process_queue)
         self.audio_thread.daemon = True
         self.audio_thread.start()
 
-        self.audio_files = queue.Queue()
         self.play_thread = threading.Thread(target=self.play_sequentially)
         self.play_thread.daemon = True
         self.play_thread.start()
+
+        self.tts_lock = threading.Lock()
+        self.play_lock = threading.Lock()
 
     def text_to_speech(self, text):
         # Synthesize speech from the text
@@ -50,10 +55,11 @@ class AudioOutput:
 
         response = self.client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
 
-        filename = f"temp_audio_{int(time.time())}.wav"
+        filename = f"tmp/audio/temp_audio_{uuid.uuid4()}.wav"
+
         with open(filename, 'wb') as out:
             out.write(response.audio_content)
-        self.audio_files.put(filename)
+        return filename
     
     def add_to_queue(self, text):
         self.request_queue.put(text)
@@ -61,24 +67,22 @@ class AudioOutput:
     def process_queue(self):
         while True:
             text = self.request_queue.get()
-            self.text_to_speech(text)
-            self.request_queue.task_done()
+            with self.tts_lock:
+                filename = self.text_to_speech(text)
+                self.ready_files.put(filename)
+                self.request_queue.task_done()
     
     def play_sequentially(self):
         while True:
-            if not self.is_playing() and not self.audio_files.empty():
-                filename = self.audio_files.get()
-
-                # Check if the file exists and retry a few times if it doesn't
-                for _ in range(3):  # retry up to 3 times
-                    if os.path.exists(filename):
-                        self.play_audio_file(filename)
-                        break
-                    time.sleep(0.1)  # wait for 100 milliseconds before retrying
-
-                # Remove the file after playing
-                if os.path.exists(filename):
-                    os.remove(filename)
+            if not self.is_playing() and not self.ready_files.empty():
+                filename = self.ready_files.get()
+                with self.play_lock:
+                    # Check if the file exists and retry a few times if it doesn't
+                    for _ in range(100):  # retry up to 3 times
+                        if os.path.exists(filename):
+                            self.play_audio_file(filename)
+                            break
+                        time.sleep(0.1)  # wait for 100 milliseconds before retrying
 
     def is_playing(self):
         return pygame.mixer.music.get_busy()
@@ -92,8 +96,6 @@ class AudioOutput:
         except pygame.error as e:
             print(f"Error playing audio file: {e}")
 
-        os.remove(filename)
-
     def stop_audio(self):
         # Stops any ongoing audio playback
         pygame.mixer.music.stop()
@@ -102,21 +104,36 @@ class AudioOutput:
         self.stop_audio()
         self.clear_audio_files_queue()
         self.clear_queue()
+        self.clear_orphaned_audio_files()
 
     def clear_audio_files_queue(self):
-        while not self.audio_files.empty():
-            filename = self.audio_files.get()
+        while not self.ready_files.empty():
+            filename = self.ready_files.get()
             try:
                 os.remove(filename)
             except OSError as e:
                 print(f"Error removing file {filename}: {e}")
-            self.audio_files.task_done()
+            self.ready_files.task_done()
+    
+    def clear_orphaned_audio_files(self):
+        # Clear any audio files that may have been left behind
+        for filename in os.listdir('tmp/audio'):
+            try:
+                os.remove("tmp/audio/"+filename)
+            except OSError as e:
+                print(f"Error removing file {filename}: {e}")
 
     def clear_queue(self):
         while not self.request_queue.empty():
             try:
                 self.request_queue.get_nowait()  # Remove all items from the queue
                 self.request_queue.task_done()
+            except queue.Empty:
+                break
+        while not self.ready_files.empty():
+            try:
+                self.ready_files.get_nowait()  # Remove all items from the queue
+                self.ready_files.task_done()
             except queue.Empty:
                 break
 
