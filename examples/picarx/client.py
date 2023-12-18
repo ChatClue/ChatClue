6577334d846fba56c5a6859dbb52f1400f4981d7
Anthony-Dev <occamsfreelance@gmail.com>
2023-12-17 18:32:38 -1000

    Adds the examples directory with an initial picarx example as a peripheral consuming the websocket.

---
+++ examples/picarx/client.py
@@ -0,0 +1,43 @@
+import asyncio
+import websockets
+import json
+from .movement import PiCarXMovements
+
+async def listen():
+    uri = "ws://localhost:8765"
+    car = PiCarXMovements()
+
+    async with websockets.connect(uri) as websocket:
+        print(f"Connected to WebSocket server at {uri}")
+        while True:
+            message = await websocket.recv()
+            print(f"Message received: {message}")
+            process_command(car, message)
+
+def process_command(car, message):
+    try:
+        command = json.loads(message)
+        action = command.get("action")
+        if action == "move_forward":
+            car.move_forward(command.get("speed", 0))
+        elif action == "move_backward":
+            car.move_backward(command.get("speed", 0))
+        elif action == "turn_left":
+            car.turn_left(command.get("speed", 0), command.get("angle", 0))
+        elif action == "turn_right":
+            car.turn_right(command.get("speed", 0), command.get("angle", 0))
+        elif action == "tilt_head_up":
+            car.tilt_head_up(command.get("angle_increment", 0))
+        elif action == "tilt_head_down":
+            car.tilt_head_down(command.get("angle_increment", 0))
+        elif action == "turn_head_left":
+            car.turn_head_left(command.get("angle_increment", 0))
+        elif action == "turn_head_right":
+            car.turn_head_right(command.get("angle_increment", 0))
+        elif action == "stop":
+            car.stop()
+    except json.JSONDecodeError:
+        print("Not a JSON command")
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(listen())
---
+++ examples/picarx/movement/movement.py
@@ -0,0 +1,47 @@
+from picarx import Picarx
+
+class PiCarXMovements:
+    def __init__(self):
+        self.px = Picarx()
+        self.pan_angle = 0
+        self.tilt_angle = 0
+
+    def move_forward(self, speed):
+        self.px.set_dir_servo_angle(0)
+        self.px.forward(speed)
+
+    def move_backward(self, speed):
+        self.px.set_dir_servo_angle(0)
+        self.px.backward(speed)
+
+    def turn_left(self, speed, angle):
+        self.px.set_dir_servo_angle(-angle)
+        self.px.forward(speed)
+
+    def turn_right(self, speed, angle):
+        self.px.set_dir_servo_angle(angle)
+        self.px.forward(speed)
+
+    def tilt_head_up(self, angle_increment):
+        self.tilt_angle += angle_increment
+        self.tilt_angle = min(self.tilt_angle, 35)
+        self.px.set_cam_tilt_angle(self.tilt_angle)
+
+    def tilt_head_down(self, angle_increment):
+        self.tilt_angle -= angle_increment
+        self.tilt_angle = max(self.tilt_angle, -35)
+        self.px.set_cam_tilt_angle(self.tilt_angle)
+
+    def turn_head_left(self, angle_increment):
+        self.pan_angle -= angle_increment
+        self.pan_angle = max(self.pan_angle, -35)
+        self.px.set_cam_pan_angle(self.pan_angle)
+
+    def turn_head_right(self, angle_increment):
+        self.pan_angle += angle_increment
+        self.pan_angle = min(self.pan_angle, 35)
+        self.px.set_cam_pan_angle(self.pan_angle)
+
+    def stop(self):
+        self.px.set_dir_servo_angle(0)
+        self.px.stop()
