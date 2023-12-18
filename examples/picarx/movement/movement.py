51fb847392b5fa6abf99cbb14c35eb3b554157b8
Anthony-Dev <occamsfreelance@gmail.com>
2023-12-17 18:59:12 -1000

    Update the picar example to ensure the car automatically stops for the example.

--- examples/picarx/movement/movement.py
+++ examples/picarx/movement/movement.py
@@ -5,22 +5,33 @@ class PiCarXMovements:
         self.px = Picarx()
         self.pan_angle = 0
         self.tilt_angle = 0
+        self.move_timer = None
+
+    def _set_timer(self):
+        if self.move_timer is not None:
+            self.move_timer.cancel()
+        self.move_timer = threading.Timer(0.5, self.stop)
+        self.move_timer.start()
 
     def move_forward(self, speed):
         self.px.set_dir_servo_angle(0)
         self.px.forward(speed)
+        self._set_timer()
 
     def move_backward(self, speed):
         self.px.set_dir_servo_angle(0)
         self.px.backward(speed)
+        self._set_timer()
 
     def turn_left(self, speed, angle):
         self.px.set_dir_servo_angle(-angle)
         self.px.forward(speed)
+        self._set_timer()
 
     def turn_right(self, speed, angle):
         self.px.set_dir_servo_angle(angle)
         self.px.forward(speed)
+        self._set_timer()
 
     def tilt_head_up(self, angle_increment):
         self.tilt_angle += angle_increment
@@ -43,5 +54,8 @@ class PiCarXMovements:
         self.px.set_cam_pan_angle(self.pan_angle)
 
     def stop(self):
+        if self.move_timer is not None:
+            self.move_timer.cancel()
+            self.move_timer = None
         self.px.set_dir_servo_angle(0)
         self.px.stop()
