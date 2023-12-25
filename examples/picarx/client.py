6505d5122616fb4a11a56b82736c659f80a16b41
Anthony-Dev <occamsfreelance@gmail.com>
2023-12-24 16:05:50 -1000

    Implement retry logic in the examples/picarx robot example to allow for continuous checks against the websocket connection.

--- examples/picarx/client.py
+++ examples/picarx/client.py
@@ -8,19 +8,27 @@ async def listen():
     uri = "ws://192.168.86.38:8765/websocket"
     car = PiCarXMovements()
 
-    async with websockets.connect(uri) as websocket:
-        print(f"Connected to WebSocket server at {uri}")
-        while True:
-            message = await websocket.recv()
-            print(f"Message received: {message}")
-            process_command(car, message)
+    while True:
+        try:
+            async with websockets.connect(uri) as websocket:
+                print(f"Connected to WebSocket server at {uri}")
+                while True:
+                    message = await websocket.recv()
+                    print(f"Message received: {message}")
+                    process_command(car, message)
+        except websockets.ConnectionClosed:
+            print("WebSocket connection closed. Reconnecting...")
+            await asyncio.sleep(5)  # Wait 5 seconds before trying to reconnect
+        except Exception as e:
+            print(f"Connection failed: {e}. Retrying in 5 seconds...")
+            await asyncio.sleep(5)  # Wait 5 seconds before trying to reconnect
 
 def process_command(car, message):
     try:
         command = json.loads(message)
         if isinstance(command, dict):
             action = command.get("action")
-            time = command.get("time", 1) / 1000  # Convert milliseconds to seconds
+            time_val = command.get("time", 1) / 1000  # Convert milliseconds to seconds
             speed = command.get("speed", 0)
             angle = command.get("angle", 0)
             tilt_increment = command.get("tilt_increment", 0)
@@ -28,10 +36,8 @@ def process_command(car, message):
 
             if action in ["move_forward", "move_backward"]:
                 direction = "forward" if action == "move_forward" else "backward"
-                car.move(direction, speed, angle, time)
+                car.move(direction, speed, angle, time_val)
             elif action == "move_head":
-                tilt_increment = tilt_increment
-                pan_increment = pan_increment
                 car.move_head(tilt_increment, pan_increment)
             elif action == "stop":
                 car.stop()
